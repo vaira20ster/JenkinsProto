0475784bc3fecd93f8aaa2e33379eace19ae807a
Vairamuthu <vairamuthu@solitontech.com>
2021-09-29 12:34:29 +0530

    added engine

--- ConsoleApp16/Engine/Program.cs
+++ ConsoleApp16/Engine/Program.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Reusable;
 
-namespace ConsoleApp15
+namespace Engine
 {
     class Program
     {
         static void Main(string[] args)
         {
             //fsdf
-            Class1 a = new Class1();
         }
     }
 }
